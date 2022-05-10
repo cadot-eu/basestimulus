428a2f5ab047ee23dd72ea84a557b4b9a1dfc3c6
Michael <m@cadot.eu>
2022-05-10 18:41:48 +0200

    add simplelanguage

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -19,6 +19,9 @@ export default class extends Controller {
         this.element.id = uniqid
         if (this.toolbarValue == 'simple')
             this.editor = simple(this.element);
+        else if
+            (this.toolbarValue == 'simplelanguage')
+            this.editor = simplelanguage(this.element);
         else
             BuildEditor.create(this.element, {
                 language: {
@@ -99,6 +102,61 @@ function simple(e) {
         });
 }
 
-
+function simplelanguage(e) {
+    return BuildEditor.create(e,
+        {
+            restrictedEditing: {
+                allowedCommands: ['highlight',
+                    'specialCharacters',],
+                allowedAttributes: ['highlight',
+                    'specialCharacters']
+            },
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                ]
+            },
+            highlight: {
+                options: [
+                    {
+                        model: 'Marker',
+                        class: '',
+                        title: 'Marqueur',
+                        type: 'marker'
+                    }
+                ]
+            },
+            toolbar: {
+                items: [
+                    'highlight',
+                    'specialCharacters',
+                    'textPartLanguage'
+                ]
+            },
+            language: {
+                textPartLanguage: [
+                    { title: 'French', languageCode: 'fr' },
+                    { title: 'Anglais', languageCode: 'en' }
+                ]
+            },
+        })
+        .then(editor => {
+            editor.setData(e.value)
+            editor.editing.view.document.on('clipboardInput', (evt, data) => {
+                data.content = editor.data.htmlProcessor.toView(data.dataTransfer.getData('text/plain'));
+            });
+            editor.model.document.on('change:data', () => {
+                e.value = editor.getData().replace(/<p>+/, "").replace(/<\/p>+$/, "");
+            });
+        })
+        .catch(error => {
+            console.error(error.stack);
+        });
+}
 
 

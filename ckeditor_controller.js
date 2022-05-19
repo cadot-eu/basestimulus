2542ce24693f8dd86c401fe773d08962071272f3
Michael <m@cadot.eu>
2022-05-19 13:20:15 +0200

    big maj

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -17,39 +17,21 @@ export default class extends Controller {
     connect() {
         let uniqid = (new Date()).getTime();
         this.element.id = uniqid
+        if (this.toolbarValue == 'full')
+            this.editor = full(this.element);
+        if (this.toolbarValue == 'simplelanguage')
+            this.editor = simplelanguage(this.element);
+        if (this.toolbarValue == 'normal')
+            this.editor = normal(this.element);
         if (this.toolbarValue == 'simple')
             this.editor = simple(this.element);
-        else if
-            (this.toolbarValue == 'simplelanguage')
-            this.editor = simplelanguage(this.element);
-        else
-            BuildEditor.create(this.element, {
-                language: {
-                    textPartLanguage: [
-                        { title: 'French', languageCode: 'fr' },
-                        { title: 'Anglais', languageCode: 'en' }
-                    ]
-                },
-                simpleUpload: {
-                    // The URL that the images are uploaded to.
-                    uploadUrl: "/upload/toto",
-
-                    // Enable the XMLHttpRequest.withCredentials property if required.
-                    withCredentials: false,
-
-                    // Headers sent along with the XMLHttpRequest to the upload server.
-                    headers: {
-                        "X-CSRF-TOKEN": "CSFR-Token",
-                        Authorization: "Bearer [JSON Web Token]"
-                    }
-                },
-            })
 
     }
     disconnect() {
 
     }
 }
+
 
 function simple(e) {
     return BuildEditor.create(e,
@@ -85,8 +67,7 @@ function simple(e) {
                     'highlight',
                     'specialCharacters'
                 ]
-            },
-            language: 'fr'
+            }
         })
         .then(editor => {
             editor.setData(e.value)
@@ -101,7 +82,6 @@ function simple(e) {
             console.error(error.stack);
         });
 }
-
 function simplelanguage(e) {
     return BuildEditor.create(e,
         {
@@ -157,6 +137,101 @@ function simplelanguage(e) {
         .catch(error => {
             console.error(error.stack);
         });
+}
+function normal(e) {
+    return BuildEditor.create(e,
+        {
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                ]
+            },
+            highlight: {
+                options: [
+                    {
+                        model: 'Marker',
+                        class: '',
+                        title: 'Marqueur',
+                        type: 'marker'
+                    }
+                ]
+            },
+            toolbar: {
+                items: [
+                    'heading',
+                    '|',
+                    'bold',
+                    'italic',
+                    'link',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'outdent',
+                    'indent',
+                    '|',
+                    'imageUpload',
+                    'blockQuote',
+                    'insertTable',
+                    'mediaEmbed',
+                    'undo',
+                    'redo',
+                    'alignment',
+                    'horizontalLine',
+                    'imageInsert',
+                    'pageBreak',
+                    'removeFormat',
+                    'specialCharacters',
+                    'strikethrough',
+                    'subscript',
+                    'superscript',
+                    'textPartLanguage',
+                    'todoList',
+                    'underline',
+                    '|'
+                ]
+            },
+            language: 'fr'
+        })
+        .then(editor => {
+            editor.setData(e.value)
+            editor.editing.view.document.on('clipboardInput', (evt, data) => {
+                data.content = editor.data.htmlProcessor.toView(data.dataTransfer.getData('text/plain'));
+            });
+            editor.model.document.on('change:data', () => {
+                e.value = editor.getData();//.replace(/<p>+/, "").replace(/<\/p>+$/, "");
+            });
+        })
+        .catch(error => {
+            console.error(error.stack);
+        });
+}
+function full(e) {
+    return BuildEditor.create(this.element, {
+        language: {
+            textPartLanguage: [
+                { title: 'French', languageCode: 'fr' },
+                { title: 'Anglais', languageCode: 'en' }
+            ]
+        },
+        simpleUpload: {
+            // The URL that the images are uploaded to.
+            uploadUrl: "/upload/toto",
+
+            // Enable the XMLHttpRequest.withCredentials property if required.
+            withCredentials: false,
+
+            // Headers sent along with the XMLHttpRequest to the upload server.
+            headers: {
+                "X-CSRF-TOKEN": "CSFR-Token",
+                Authorization: "Bearer [JSON Web Token]"
+            }
+        },
+    })
 }
 
 

b562af4e2d62c82a7ce979b5bbb2cdb86716b6a3
Michael <m@cadot.eu>
2022-06-02 10:34:28 +0200

    new chart

--- chart_controller.js
+++ chart_controller.js
@@ -1,10 +1,102 @@
-import { Application } from 'stimulus'
-import Chart from 'stimulus-chartjs'
+import { Controller } from '@hotwired/stimulus'
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+} from 'chart.js';
 
-const application = Application.start()
-application.register('chart', Chart)
+Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+);
 
 /* -------------------------------------------------------------------------- */
 /*                                  variables                                 */
 /* -------------------------------------------------------------------------- */
 // data-chart-data-value=...
+
+export default class extends Controller {
+
+    static values = {
+        data: Array,
+        pointer: String
+    }
+    /* -------------------------------------------------------------------------- */
+    /*                                     use                                    */
+    /* -------------------------------------------------------------------------- */
+    connect() {
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: this.dataValue,
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+        const myChart = new Chart(
+            this.element,
+            config
+        );
+    }
+
+}

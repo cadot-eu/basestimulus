dadf103e3870d46dbf58f902b61388b54701d7fd
Michael <m@cadot.eu>
2022-06-23 01:53:14 +0200

    parametres

---
+++ disabledroot_controller.js
@@ -0,0 +1,20 @@
+
+import { Controller } from '@hotwired/stimulus'
+export default class extends Controller {
+    /* -------------------------------------------------------------------------- */
+    /*                              variable and uses                             */
+    /* -------------------------------------------------------------------------- */
+    static values = {
+        code: String
+    }
+
+
+
+    connect() {
+        if (this.codeValue == 'm@cadot.eu') {
+            this.element.setAttribute('disabled', true)
+        }
+    }
+
+
+}

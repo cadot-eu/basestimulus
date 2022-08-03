341c61d10c0961b1645c3034a19ca2c25e2e428a
Michael <m@cadot.eu>
2022-08-03 15:45:44 +0200

    rectif objects

--- carousel_controller.js
+++ carousel_controller.js
@@ -1,3 +1,27 @@
+//exemple d'utilisation
+{/* <div class="swiper sliderFeaturedPosts" data-base--carousel-options-value='{"navigation": {"nextEl":".custom-swiper-button-next","prevEl": ".custom-swiper-button-prev" }}' data-controller="base--carousel">
+    <div class="swiper-wrapper">
+        {% for item in carousels %}
+            <div class="swiper-slide">
+                <div class="swiper-lazy img-bg d-flex align-items-end" data-background="{{ asset(item.image)|imagine_filter('bandeaufixe') }}">
+                    <div class="img-bg-inner bg-dark bg-opacity-50 p-3 ms-5">
+                        <h2>{{ item.titre | raw }}</h2>
+                        <p class="h4">{{ item.texte | raw }}</p>
+                    </div>
+                    <div class="swiper-lazy-preloader"></div>
+                </div>
+            </div>
+        {% endfor %}
+    </div>
+    <div class="custom-swiper-button-next">
+        <span class="bi-chevron-right"></span>
+    </div>
+    <div class="custom-swiper-button-prev">
+        <span class="bi-chevron-left"></span>
+    </div>
+    <div class="swiper-pagination"></div>
+</div> */}
+
 import { Controller } from '@hotwired/stimulus'
 // import Swiper JS
 import Swiper from 'swiper/bundle';
--- chart_controller.js
+++ chart_controller.js
@@ -1,3 +1,7 @@
+//exemple d'utilisation
+//<canvas data-controller="base--chart" data-base--chart-type-value="bar" data-base--chart-data-value='[37, 83, 78, 54, 12, 5, 99]' data-base--chart-label-value=' ["january", "february", "march", "april", "may", "june"]' data-base--chart-param-value='{"label": "My First dataset", "backgroundColor": "rgb(255, 99, 132)", "borderColor": "rgb(255, 99, 132)"}' "></canvas>
+
+
 import { Controller } from '@hotwired/stimulus'
 import {
     Chart,
@@ -62,35 +66,29 @@ Chart.register(
 export default class extends Controller {
 
     static values = {
+        nom: String,
         data: Array,
+        label: Array,
+        param: Object,
+        type: { type: String, default: 'line' }, //type de graphique line, bar, pie, doughnut, polarArea, radar, scatter...
         pointer: String
     }
     /* -------------------------------------------------------------------------- */
     /*                                     use                                    */
     /* -------------------------------------------------------------------------- */
     connect() {
-        const labels = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-        ];
 
-        const data = {
-            labels: labels,
+        const donnees = {
+            labels: this.labelValue,
             datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: this.dataValue,
+                ...this.paramValue,
+                data: this.dataValue
             }]
         };
 
         const config = {
-            type: 'line',
-            data: data,
+            type: this.typeValue,
+            data: donnees,
             options: {}
         };
         const myChart = new Chart(

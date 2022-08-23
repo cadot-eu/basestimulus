59542ad60c2c46049fe187d1de1561541dd1e0ea
Michael <m@cadot.eu>
2022-08-23 14:50:04 +0200

    debut creer comparatif

--- collection_controller.js
+++ collection_controller.js
@@ -1,3 +1,4 @@
+//ajoute bigpicture aux images dans le data-controller avec une image de la valeur du champ
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
@@ -6,20 +7,20 @@ export default class extends Controller {
     }
 
     connect() {
-        const proto = this.element.getAttribute('data-prototype')
-        const data = JSON.parse(this.valeursValue)
-        this.element.querySelectorAll('input[type=file]').forEach((element, key) => {
-            if (element.accept == 'image/*') {
-                const img = document.createElement("img");
-                img.src = '/' + data[key]
-                img.setAttribute('data-controller', 'base--bigpicture')
-                img.width = 64;
-                element.after(img)
-            }
-            else
-                element.after(data[key].split('¤')[0].split('/').at(-1))
-            element.setAttribute('exurl', '/' + data[key])
-        });
+        // const proto = this.element.getAttribute('data-prototype')
+        // const data = JSON.parse(this.valeursValue)
+        // this.element.querySelectorAll('input[type=file]').forEach((element, key) => {
+        //     if (element.accept == 'image/*') {
+        //         const img = document.createElement("img");
+        //         img.src = '/' + data[key]
+        //         img.setAttribute('data-controller', 'base--bigpicture')
+        //         img.width = 64;
+        //         element.after(img)
+        //     }
+        //     else
+        //         element.after(data[key].split('¤')[0].split('/').at(-1))
+        //     element.setAttribute('exurl', '/' + data[key])
+        // });
     }
 
 

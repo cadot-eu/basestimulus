13cd45d7d9acfb721d6ffcea33180ed67845484d
Michael <m@cadot.eu>
2022-11-01 10:06:02 +0100

    suppression du formulaire commenatire

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -19,6 +19,7 @@ export default class extends Controller {
 
 
     connect() {
+        console.log(this.toolbarValue)
         if (this.toolbarValue == 'full')
             this.editor = full(this);
         else if (this.toolbarValue == 'simplelanguage')

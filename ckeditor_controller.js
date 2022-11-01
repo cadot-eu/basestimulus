6bcd8d99142c6bdf78cb1229c75bfa7d528804e1
Michael <m@cadot.eu>
2022-11-01 11:11:58 +0100

    maj parametres

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -19,7 +19,6 @@ export default class extends Controller {
 
 
     connect() {
-        console.log(this.toolbarValue)
         if (this.toolbarValue == 'full')
             this.editor = full(this);
         else if (this.toolbarValue == 'simplelanguage')

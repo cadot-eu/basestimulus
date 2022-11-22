1b8d2c70728b958cad12898a9dd95560b08a35f9
Michael <m@cadot.eu>
2022-11-22 19:50:55 +0100

    refactorisation en cours

--- collapse_controller.js
+++ collapse_controller.js
@@ -1,5 +1,14 @@
-//stimulus qui renvoie de l'html donné par une url par ajax.
-
+//permet sur un click de changer l'html de l'élément
+//
+// exemple
+{/*
+ <p data-controller="base--collapse" data-base--collapse-out-value="{{ item.reponse }}" data-action="click->base--collapse#click" style="cursor:pointer;">
+    {{ item.reponse|raw|striptags|u.truncate(30, '...') }}
+    <span>
+        <i class="text-muted">Cliquez pour lire la réponse</i>
+    </span>
+</p> 
+*/}
 import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     static values = {
@@ -10,8 +19,6 @@ export default class extends Controller {
 
     click() {
         this.element.innerHTML = this.outValue;
-        this.element.value = "toto"
-        console.log('click')
     }
 
 

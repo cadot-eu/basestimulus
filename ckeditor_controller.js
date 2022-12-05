93566bde955ca14d1992e6ff2746f6303f2d2f4b
Michael <m@cadot.eu>
2022-12-05 20:16:42 +0100

    resize image en attendant le nouvel editeur

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -3,6 +3,7 @@ import BuildEditor from '/assets/ckeditor/build/ckeditor';
 const { filetemplates } = require('/assets/simpleBoxTemplates.js').default;
 let templates = JSON.parse(filetemplates);
 let liipFilters;
+let liipItems;
 
 /* ---------------- transformation des textareas en fckeditor --------------- */
 
@@ -20,20 +21,30 @@ export default class extends Controller {
 
 
     async connect() {
-        const response = await fetch('/admin/getLiipFilters');
-        const data = await response.json();
+        // const response = await fetch('/admin/getLiipFilters');
+        // const data = await response.json();
+        // let tablo = [];
+        // let tabloitem = [];
+        // data.forEach(element => {
+        //     if (!Number.isInteger(element) && element !== 'cache' && element !== 'label') {
+        //         tablo.push({ name: element, title: element, label: element, className: element, modelElements: ['imageBlock'] });
+        //         tabloitem.push('imageStyle:' + element);
+        //     }
+        // });
+        // liipFilters = tablo;
+        // liipItems = tabloitem;
         let tablo = [{
             name: 'resizeImage:original',
             value: null,
             label: 'Original'
         },];
+        const data = ['carousel', 'bandeaufixe', 'petitbanderole', 'hd', 'moyencarree', 'petitcarree', 'minicarree', 'icone'];
         data.forEach(element => {
             if (!Number.isInteger(element) && element !== 'cache') {
                 tablo.push({ name: element, value: element, label: element });
             }
         });
         liipFilters = tablo;
-        console.log(liipFilters);
 
         if (this.toolbarValue == 'full')
             this.editor = full(this);
@@ -205,6 +216,7 @@ function simplelanguage(e) {
 function normal(e) {
     return BuildEditor.create(e.element,
         {
+
             heading: {
                 options: [
                     { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
@@ -236,7 +248,7 @@ function normal(e) {
             },
             toolbar: {
                 items: [
-                    'simpleBox',
+                    // 'simpleBox',
                     'heading',
                     'bold',
                     'italic',
@@ -269,9 +281,10 @@ function normal(e) {
                     '|'
                 ]
             },
-            simpleBox: {
-                templates: templates
-            },
+            // simpleBox: {
+            //     templates: templates,
+            //     debug: "true"
+            // },
             simpleUpload: {
                 // The URL that the images are uploaded to.
                 uploadUrl: "/upload/" + e.uploadValue,
@@ -290,19 +303,51 @@ function normal(e) {
                     { title: 'Anglais', languageCode: 'en' }
                 ]
             },
-
             image: {
                 resizeOptions: liipFilters
                 ,
                 toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight', 'toggleImageCaption', 'imageStyle:linkImage', 'resizeImage'],
                 styles: ['full', 'alignLeft', 'alignCenter', 'alignRight', 'toggleImageCaption', 'linkImage']
-            },
-        })
+            }
+            // styles: {
+            //     // Defining custom styling options for the images.
+            //     options: liipFilters
+            //     //[{ "name": "carousel", "title": "carousel", "label": "carousel", "className": "carousel", "modelElements": ["imageInline"] },
 
+            // },
+            // toolbar: [{
+            //     // Grouping the buttons for the icon-like image styling
+            //     // into one drop-down.
+            //     name: 'imageStyle:icons',
+            //     label: 'Icons',
+            //     items: liipItems,
+            //     defaultItem: liipItems[0]
+            // }
+            //]
+        },
+        // image: {
+        //     // resizeOptions: liipFilters
+        //     // ,
+        //     toolbar: [{
+        //         // Grouping the buttons for the icon-like image styling
+        //         // into one drop-down.
+        //         name: 'imageStyle:icons',
+        //         items: [liipItems],
+        //         defaultItem: 'imageStyle:margin-left'
+        //     }, '|', 'toggleImageCaption', 'linkImage'
+        //     ],
+        //     //toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight', 'toggleImageCaption', 'imageStyle:linkImage', 'resizeImage'],
+        //     styles: { options: liipFilters }   //['full', 'alignLeft', 'alignCenter', 'alignRight', 'toggleImageCaption', 'linkImage']
+        // },
+        //}
+    )
         .then(editor => {
             editor.setData(e.element.value)
-            editor.model.document.on('change:data', () => {
-                e.element.value = editor.getData();//.replace(/<p>+/, "").replace(/<\/p>+$/, "");
+            editor.model.document.on('change:data', (evt, data) => {
+                e.element.value = editor.getData();
+                //on remet les images en grande taille
+                e.element.value = editor.getData().replace(/\media\/cache\/resolve\//g, 'media/cache/1920/');
+                //.replace(/<p>+/, "").replace(/<\/p>+$/, "");
             });
             editor.plugins.get('FileRepository').loaders.on('add', (evt, loader) => {
                 let barre = "<div id='progressUploadBar' class='progressWrapper fixed-top' style='z-index:100000;float: left; height:.3rem;width: 100%'><div id='progressUpload' class='progress' style='float: left; width: 0%; background-color: yellow'></div><div id='progressUploadText' class='progressText' style='float: left;></div></div>";
@@ -338,7 +383,34 @@ function normal(e) {
                 // to allow further processing of the content.
                 data.content = viewContent;
             });
-
+            editor.plugins.get('ImageResizeHandles').set('isEnabled', false);
+            editor.commands.get('resizeImage').on('execute', (evt, loader) => {
+                // console.log(evt)
+                if (loader[0].width !== undefined) {
+                    let width = loader[0].width.slice(0, -1);
+                    if (isNaN(width)) {
+                        let poswidth = editor.getData().indexOf('width:' + width + '%');
+                        let possrc = editor.getData().indexOf('src="', poswidth);
+                        let finsrc = editor.getData().indexOf('"', possrc + 5);
+                        let src = editor.getData().slice(possrc, finsrc);
+                        //on test si on est dans le cas d'une image liip
+                        // <img class="image_resized" style="" src="http://localhost/media/cache/resolve/petitcarree/uploads/article/image_6380c7030fcc5.png" alt="image:image"></img>
+                        if (src.indexOf('media/cache/') !== -1) {
+                            let effect = src.split('/media/cache/')[1].split('/')[0];
+                            if (effect == 'resolve') {
+                                editor.setData(editor.getData().replace(src, src.replace(src.split('/media/cache/resolve/')[1].split('/')[0], width)));
+                            }
+                            else {
+                                editor.setData(editor.getData().replace(src, src.replace(effect, width).replace('cache/', 'cache/resolve/')));
+                            }
+                        }
+                        else {
+                            editor.setData(editor.getData().replace(src, src.replace('src="', 'src="/media/cache/resolve/' + width)));
+                        }
+                    }
+                }
+            }
+            );
 
         })
         .catch(error => {

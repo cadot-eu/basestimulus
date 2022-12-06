7873649cab9efb7f3875924ab83170fcc25fa59d
Michael <m@cadot.eu>
2022-12-06 11:36:52 +0100

    en attente nouvel editeur

--- ckeditor_controller.js
+++ ckeditor_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 import BuildEditor from '/assets/ckeditor/build/ckeditor';
-const { filetemplates } = require('/assets/simpleBoxTemplates.js').default;
-let templates = JSON.parse(filetemplates);
-let liipFilters;
-let liipItems;
+
+
 
 /* ---------------- transformation des textareas en fckeditor --------------- */
 
@@ -20,32 +18,7 @@ export default class extends Controller {
     }
 
 
-    async connect() {
-        // const response = await fetch('/admin/getLiipFilters');
-        // const data = await response.json();
-        // let tablo = [];
-        // let tabloitem = [];
-        // data.forEach(element => {
-        //     if (!Number.isInteger(element) && element !== 'cache' && element !== 'label') {
-        //         tablo.push({ name: element, title: element, label: element, className: element, modelElements: ['imageBlock'] });
-        //         tabloitem.push('imageStyle:' + element);
-        //     }
-        // });
-        // liipFilters = tablo;
-        // liipItems = tabloitem;
-        let tablo = [{
-            name: 'resizeImage:original',
-            value: null,
-            label: 'Original'
-        },];
-        const data = ['carousel', 'bandeaufixe', 'petitbanderole', 'hd', 'moyencarree', 'petitcarree', 'minicarree', 'icone'];
-        data.forEach(element => {
-            if (!Number.isInteger(element) && element !== 'cache') {
-                tablo.push({ name: element, value: element, label: element });
-            }
-        });
-        liipFilters = tablo;
-
+    connect() {
         if (this.toolbarValue == 'full')
             this.editor = full(this);
         else if (this.toolbarValue == 'simplelanguage')
@@ -57,11 +30,12 @@ export default class extends Controller {
         else if (this.toolbarValue == 'simple')
             this.editor = simple(this);
         else this.editor = vide(this)
-
-
+        //protection contre le problème required sur un champ display none qui cré l'erreur is not focusable 
     }
 
+    disconnect() {
 
+    }
 }
 
 
@@ -203,7 +177,7 @@ function simplelanguage(e) {
         .then(editor => {
             editor.setData(e.element.value)
             editor.editing.view.document.on('clipboardInput', (evt, data) => {
-
+                data.content = editor.data.htmlProcessor.toView(data.dataTransfer.getData('text/plain'));
             });
             editor.model.document.on('change:data', () => {
                 e.element.value = editor.getData();//.replace(/<p>+/, "").replace(/<\/p>+$/, "");
@@ -216,7 +190,6 @@ function simplelanguage(e) {
 function normal(e) {
     return BuildEditor.create(e.element,
         {
-
             heading: {
                 options: [
                     { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
@@ -248,7 +221,8 @@ function normal(e) {
             },
             toolbar: {
                 items: [
-                    // 'simpleBox',
+                    'simplebox',
+                    'placeholder',
                     'heading',
                     'bold',
                     'italic',
@@ -267,6 +241,7 @@ function normal(e) {
                     'imageUpload',
                     'blockQuote',
                     'insertTable',
+                    'mediaEmbed',
                     'link',
                     'todoList',
                     'undo',
@@ -281,13 +256,11 @@ function normal(e) {
                     '|'
                 ]
             },
-            // simpleBox: {
-            //     templates: templates,
-            //     debug: "true"
-            // },
+
+
             simpleUpload: {
                 // The URL that the images are uploaded to.
-                uploadUrl: "/upload/" + e.uploadValue,
+                uploadUrl: "/simplegallery/" + e.uploadValue,
                 // Enable the XMLHttpRequest.withCredentials property if required.
                 withCredentials: true,
 
@@ -303,66 +276,41 @@ function normal(e) {
                     { title: 'Anglais', languageCode: 'en' }
                 ]
             },
-            image: {
-                resizeOptions: liipFilters
-                ,
-                toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight', 'toggleImageCaption', 'imageStyle:linkImage', 'resizeImage'],
-                styles: ['full', 'alignLeft', 'alignCenter', 'alignRight', 'toggleImageCaption', 'linkImage']
-            }
-            // styles: {
-            //     // Defining custom styling options for the images.
-            //     options: liipFilters
-            //     //[{ "name": "carousel", "title": "carousel", "label": "carousel", "className": "carousel", "modelElements": ["imageInline"] },
+        })
 
-            // },
-            // toolbar: [{
-            //     // Grouping the buttons for the icon-like image styling
-            //     // into one drop-down.
-            //     name: 'imageStyle:icons',
-            //     label: 'Icons',
-            //     items: liipItems,
-            //     defaultItem: liipItems[0]
-            // }
-            //]
-        },
-        // image: {
-        //     // resizeOptions: liipFilters
-        //     // ,
-        //     toolbar: [{
-        //         // Grouping the buttons for the icon-like image styling
-        //         // into one drop-down.
-        //         name: 'imageStyle:icons',
-        //         items: [liipItems],
-        //         defaultItem: 'imageStyle:margin-left'
-        //     }, '|', 'toggleImageCaption', 'linkImage'
-        //     ],
-        //     //toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight', 'toggleImageCaption', 'imageStyle:linkImage', 'resizeImage'],
-        //     styles: { options: liipFilters }   //['full', 'alignLeft', 'alignCenter', 'alignRight', 'toggleImageCaption', 'linkImage']
-        // },
-        //}
-    )
         .then(editor => {
             editor.setData(e.element.value)
-            editor.model.document.on('change:data', (evt, data) => {
-                e.element.value = editor.getData();
-                //on remet les images en grande taille
-                e.element.value = editor.getData().replace(/\media\/cache\/resolve\//g, 'media/cache/1920/');
-                //.replace(/<p>+/, "").replace(/<\/p>+$/, "");
+            // editor.editing.view.document.on('clipboardInput', (evt, data) => {
+            //     data.content = editor.data.htmlProcessor.toView(data.dataTransfer.getData('text/plain'));
+            // });
+            editor.model.document.on('change:data', () => {
+                e.element.value = editor.getData();//.replace(/<p>+/, "").replace(/<\/p>+$/, "");
             });
             editor.plugins.get('FileRepository').loaders.on('add', (evt, loader) => {
+                //console.log('Added an upload loder', loader);
+                // console.log('The file is', loader.file);
+                //loader.file = scaleImage(loader.file)
                 let barre = "<div id='progressUploadBar' class='progressWrapper fixed-top' style='z-index:100000;float: left; height:.3rem;width: 100%'><div id='progressUpload' class='progress' style='float: left; width: 0%; background-color: yellow'></div><div id='progressUploadText' class='progressText' style='float: left;></div></div>";
-                let elem = document.createElement('div');
+                var elem = document.createElement('div');
                 elem.innerHTML = barre;
                 document.body.appendChild(elem);
                 loader.on('change:uploadedPercent', (evt, name, uploadedPercent) => {
                     document.getElementById('progressUpload').style.width = uploadedPercent + '%';
                     if (uploadedPercent == 100) {
                         document.getElementById('progressUploadBar').remove()
+                        console.log(loader.file)
+                        const range = editor.model.createRangeIn(editor.model.document.getRoot());
+
+                        for (const item of range.getItems()) {
+                            if (item.getAttribute('uploadId') === loader.id) {
+                                //console.log('Model element of uploaded image', item);
+                            }
+                        }
                     }
                 });
 
                 loader.on('change:status', (evt, name, status) => {
-                    console.log(`Upload status: ${status}`);
+                    //console.log(`Upload status: ${status}`);
                 });
             });
             toolbar(editor)
@@ -383,35 +331,6 @@ function normal(e) {
                 // to allow further processing of the content.
                 data.content = viewContent;
             });
-            editor.plugins.get('ImageResizeHandles').set('isEnabled', false);
-            editor.commands.get('resizeImage').on('execute', (evt, loader) => {
-                // console.log(evt)
-                if (loader[0].width !== undefined) {
-                    let width = loader[0].width.slice(0, -1);
-                    if (isNaN(width)) {
-                        let poswidth = editor.getData().indexOf('width:' + width + '%');
-                        let possrc = editor.getData().indexOf('src="', poswidth);
-                        let finsrc = editor.getData().indexOf('"', possrc + 5);
-                        let src = editor.getData().slice(possrc, finsrc);
-                        //on test si on est dans le cas d'une image liip
-                        // <img class="image_resized" style="" src="http://localhost/media/cache/resolve/petitcarree/uploads/article/image_6380c7030fcc5.png" alt="image:image"></img>
-                        if (src.indexOf('media/cache/') !== -1) {
-                            let effect = src.split('/media/cache/')[1].split('/')[0];
-                            if (effect == 'resolve') {
-                                editor.setData(editor.getData().replace(src, src.replace(src.split('/media/cache/resolve/')[1].split('/')[0], width)));
-                            }
-                            else {
-                                editor.setData(editor.getData().replace(src, src.replace(effect, width).replace('cache/', 'cache/resolve/')));
-                            }
-                        }
-                        else {
-                            editor.setData(editor.getData().replace(src, src.replace('src="', 'src="/media/cache/resolve/' + width)));
-                        }
-                    }
-                }
-            }
-            );
-
         })
         .catch(error => {
             console.error(error.stack);
@@ -440,6 +359,9 @@ function full(e) {
     })
         .then(editor => {
             editor.setData(e.element.value)
+            // editor.editing.view.document.on('clipboardInput', (evt, data) => {
+            //     data.content = editor.data.htmlProcessor.toView(data.dataTransfer.getData('text/plain'));
+            // });
             editor.model.document.on('change:data', () => {
                 e.element.value = editor.getData();//.replace(/<p>+/, "").replace(/<\/p>+$/, "");
             });
@@ -453,14 +375,14 @@ function full(e) {
 function toolbar(editor) {
     if (editor.sourceElement.getAttribute('classEditor')) {
         //mise en bas de la barre d'outil
-        let toolbarfixed = document.createElement('div')
+        var toolbarfixed = document.createElement('div')
         toolbarfixed.classList.add(editor.sourceElement.getAttribute('classEditor'))
         toolbarfixed.appendChild(editor.ui.view.toolbar.element)
         toolbarfixed.classList.add('mb-5', 'pb-1')
         document.body.appendChild(toolbarfixed)
         document.body.classList.add('pb-5', 'mb-5')
         // création d'un bouton envoyer'
-        let sub = document.createElement('button')
+        var sub = document.createElement('button')
         sub.type = "submit"
         sub.classList.add('btn', 'btn-primary')
         sub.textContent = "Envoyer"

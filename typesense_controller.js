3c253c428f1df98789bc789bab92ea8ebc509cb5
michael <m@cadot.eu>
2023-01-03 17:44:19 +0100

    article recherche ok

---
+++ typesense_controller.js
@@ -0,0 +1,44 @@
+
+{/*
+ <p data-controller="base--collapse" data-base--collapse-out-value="{{ item.reponse }}" data-action="click->base--collapse#click" style="cursor:pointer;">
+    {{ item.reponse|raw|striptags|u.truncate(30, '...') }}
+    <span>
+        <i class="text-muted">Cliquez pour lire la réponse</i>
+    </span>
+</p> 
+*/}
+import { Controller } from '@hotwired/stimulus';
+const Typesense = require('typesense')
+const typesense = new Typesense.Client({
+    nodes: [
+        {
+            host: 'localhost',
+            port: '8108',
+            protocol: 'http'
+        }
+    ],
+    apiKey: 'PPkTPEfTsxtR44'
+})
+export default class extends Controller {
+    static targets = ["box", "list", "recherche"]
+    connect() {
+
+    }
+
+    async search() {
+        let searchResults = []
+        searchResults = await typesense.collections('articles').documents().search({
+            q: this.rechercheTarget.value,
+            query_by: 'titre,texte'
+        })
+        this.listTarget.innerHTML = ''
+        for (let i = 0; i < searchResults.hits.length; i++) {
+            const element = searchResults.hits[i]['document'];
+            this.listTarget.innerHTML += `<li class="list-group-item"><a href="/les-articles/${element.slug}" class="text-secondary">${element.titre}</a></li>`
+        }
+
+        this.boxTarget.classList.replace('d-none', 'd-block')
+    }
+
+
+}

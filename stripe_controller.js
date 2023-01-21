b9f726fef10bfab752aa3917b410c6d1f6ba9c68
michael <m@cadot.eu>
2023-01-21 16:48:24 +0100

    add stripe

--- stripe_controller.js
+++ stripe_controller.js
@@ -1,28 +1,41 @@
 import { Controller } from '@hotwired/stimulus';
-//import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+
+let stripe, elements, cardElement;
 
 export default class extends Controller {
     static values = {
-        target: String
+        target: String,
+        stripekey: String
     }
-    static targets = ['tokenid']
-    connect() {
-    }
-    createToken(e) {
-        let bouton = e.target
-        let tokenid = this.tokenidTarget
-        bouton.disabled = true;
-        // stripe.createToken(cardElement).then(function (result) {
-        //     if (typeof result.error != 'undefined') {
-        //         bouton.disabled = false;
-        //         alert(result.error.message);
-        //     }
+    static targets = ['bouton', 'token', 'form']
+    async connect() {
+        //creation des éléments de paiement
+        stripe = await loadStripe(this.stripekeyValue);
+        elements = stripe.elements();
+        cardElement = elements.create('card');
+        cardElement.mount('#card-element');
 
-        //     // creating token success
-        //     if (typeof result.token != 'undefined') {
-        //         tokenid.value = result.token.id;
-        //         this.element.form.requestSubmit()
-        //     }
-        // });
+    }
+    createToken() {
+        //retour du bouton de paiement
+        this.boutonTarget.disabled = true;
+        let tokenT = this.tokenTarget;
+        let formT = this.formTarget;
+        let boutonT = this.boutonTarget;
+        stripe.createToken(cardElement).then(function (result) {
+
+            if (typeof result.error != 'undefined') {
+                boutonT.disabled = false;
+                alert(result.error.message);
+            }
+
+            // creating token success
+            if (typeof result.token != 'undefined') {
+                tokenT.value = result.token.id;
+                formT.submit();
+            }
+        });
     }
 }

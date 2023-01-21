efc084e40763ea7a48583aa4dbd2009febaa0b24
michael <m@cadot.eu>
2023-01-21 15:41:37 +0100

    rm stripe

--- stripe_controller.js
+++ stripe_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
-import { loadStripe } from '@stripe/stripe-js';
+//import { loadStripe } from '@stripe/stripe-js';
 
 export default class extends Controller {
     static values = {
@@ -12,17 +12,17 @@ export default class extends Controller {
         let bouton = e.target
         let tokenid = this.tokenidTarget
         bouton.disabled = true;
-        stripe.createToken(cardElement).then(function (result) {
-            if (typeof result.error != 'undefined') {
-                bouton.disabled = false;
-                alert(result.error.message);
-            }
+        // stripe.createToken(cardElement).then(function (result) {
+        //     if (typeof result.error != 'undefined') {
+        //         bouton.disabled = false;
+        //         alert(result.error.message);
+        //     }
 
-            // creating token success
-            if (typeof result.token != 'undefined') {
-                tokenid.value = result.token.id;
-                this.element.form.requestSubmit()
-            }
-        });
+        //     // creating token success
+        //     if (typeof result.token != 'undefined') {
+        //         tokenid.value = result.token.id;
+        //         this.element.form.requestSubmit()
+        //     }
+        // });
     }
 }

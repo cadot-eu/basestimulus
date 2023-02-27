628918298b5e94a736bd43a1353652fbbe6c7195
michael <m@cadot.eu>
2023-02-27 17:58:51 +0100

    maj timetrait

---
+++ resetinput_controller.js
@@ -0,0 +1,26 @@
+//vide l'input
+//data-controller="base--resetinput"
+import { Controller } from '@hotwired/stimulus'
+export default class extends Controller {
+
+    connect() {
+        let span = document.createElement('span');
+        span.title = "cliquer pour effacer la date"
+        span.style.color = "red";
+        span.style.marginLeft = "-20px";
+        span.style.position = "absolute";
+
+
+        span.innerHTML = '<i class="bi bi-x-square"></i>';
+        this.element.parentNode.prepend(span);
+        let input = this.element.parentNode.querySelector('input');
+        span.addEventListener("click", function () {
+            input.value = "";
+
+        });
+    }
+    disconnect() {
+
+    }
+}
+

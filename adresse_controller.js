f46d806c60562ee990c605b59fe1d7131a15d7f9
michael <m@cadot.eu>
2023-05-11 08:36:33 +0200

    refactor(adresse_controller.js): supprimer les variables inutilisées 'ville' et 'codepostal' refactor(adresse_controller.js): modifier l'URL de l'API pour rechercher des adresses en utilisant le champ de saisie actuel 'that.value'

--- adresse_controller.js
+++ adresse_controller.js
@@ -4,18 +4,10 @@ import { async } from 'regenerator-runtime'
 
 let that = null;
 let timer = 0; //debunces
-let ville;
-let codepostal;
 export default class extends Controller {
-    static values = {
-        codepostal: String,
-        ville: String,
-    }
+
     connect() {
         that = this.element;
-        codepostal = this.codepostalValue;
-        ville = this.villeValue;
-        console.log(ville)
         this.element.addEventListener('input', function (e) {
             clearTimeout(timer);
             timer = setTimeout(function () {
@@ -29,8 +21,7 @@ export default class extends Controller {
 }
 
 async function fetchAsync() {
-    let response = await fetch('https://nominatim.openstreetmap.org/search?postalcode=' + document.getElementById(codepostal).value + '&street=' + that.value + '&format=json&limit=1')
-    console.log('https://nominatim.openstreetmap.org/search?postalcode=' + document.getElementById(codepostal).value + '&street=' + that.value + '&format=json&limit=1')
+    let response = await fetch('https://nominatim.openstreetmap.org/search?q=' + that.value + '&format=json&limit=5')
     let data = await response.json();
     return data;
 }

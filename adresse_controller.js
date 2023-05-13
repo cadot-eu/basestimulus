676d4a285d82018436a1caa898c926503689b3dc
michael <m@cadot.eu>
2023-05-13 17:02:29 +0200

    fonctionnalité(adresse_controller.js) : ajouter un contrôleur Stimulus pour l'autocomplétion d'adresse avec l'API adresse.data.gouv.fr. Le contrôleur permet de récupérer les adresses correspondant à la saisie de l'utilisateur et de les afficher dans un select. Le contrôleur permet également de gérer les erreurs et de remplir un champ caché avec l'adresse sélectionnée.

--- adresse_controller.js
+++ adresse_controller.js
@@ -1,38 +1,92 @@
-/* A Stimulus controller that is used to debounce the input of a form and send the form if is modified. */
+/**
+    * adresse
+    * attr:{"data-base--adresse-limit-value":15}
+    * attr:{"data-base--adresse-destination-value":"bien_adresseduselect"}
+    */
+
 import { Controller } from '@hotwired/stimulus'
-import { async } from 'regenerator-runtime'
 
 let that = null;
 let timer = 0; //debunces
+let limit;
+let message;
+let error;
+let select;
+let destination; //contient l'élément où on veut afficher si une adresse a été choisie dans le select
 export default class extends Controller {
 
+    static values = {
+        limit: { type: Number, default: 10 },
+        destination: String
+
+    }
     connect() {
+        if (this.destinationValue != null) {
+            destination = document.getElementById(this.destinationValue);
+        }
+        else {
+            destination = this.element;
+        }
+        limit = this.limitValue;
         that = this.element;
-        this.element.addEventListener('input', function (e) {
+        //create error
+        error = document.createElement('div');
+        error.classList.add('invalid-feedback');
+
+        that.insertAdjacentElement('afterend', error);
+        //create select
+        select = document.createElement('select');
+        select.classList.add('form-select');
+        select.setAttribute('aria-label', 'adresse autocomplete');
+        select.hidden = true;
+        that.insertAdjacentElement('afterend', select);
+
+        //on ajoute un event listener sur l'input
+        this.element.addEventListener('input', function (input) {
+            input.target.classList.remove('is-ok');
             clearTimeout(timer);
             timer = setTimeout(function () {
                 //on récupère les données de l'api
-                fetchAsync().then(data => {
+                fetchAsync()
+                    .then(data => {
 
-                    console.log(data);
-                    //on ajoute un select après l'input s'il existe pas sinon on modifie l'existant
-                    let options = '<option value="">Choisir une adresse</option>';
-                    data.features.forEach(element => {
-                        options += '<option value="' + element.properties.label + '">' + element.properties.label + '</option>';
-                    });
-                    if (that.nextElementSibling == null) {
-                        that.insertAdjacentHTML('afterend', '<select class="form-select" aria-label="adresse autocomplete" id="adresse_autocomplete">' + options + '</select>');
-                        //on ajoute un event listener sur le select
-                        that.nextElementSibling.addEventListener('change', function (e) {
-                            that.value = this.value;
-                            this.remove();
-                        });
+                        //si on a une erreur on l'affiche
+                        if (data.code == 400) {
+                            switch (data.message) {
+                                case "q must contain between 3 and 200 chars and start with a number or a letter":
+                                    message = "Le champ doit contenir entre 3 et 200 caractères et commencer par un chiffre ou une lettre";
+                                    break;
+                            }
+                            error.innerHTML = message;
+                            select.hidden = true;
+                            input.target.classList.add('is-invalid');
+                            input.target.classList.remove('is-valid');
+                            return;
+                        }
+                        input.target.classList.remove('is-invalid');
+                        //si on a pas de résultat on affiche un message
+                        if (data.features.length > 0) {
+                            let options = '<option value="">Choisir une adresse</option>';
+                            data.features.forEach(element => {
+                                options += '<option value="' + element.properties.label + '">' + element.properties.label + '</option>';
+                            });
+                            //on ajoute les options et on affiche le select
+                            if (select.hidden == true) {
+                                select.innerHTML = options;
+                                //on ajoute un event listener sur le select
+                                select.addEventListener('change', function (e) {
+                                    that.value = this.value;
+                                    select.hidden = true;
+                                    destination.value = input.target.value;
+                                });
+                            }
+                            else { //si le select est déjà affiché on change juste les options
+                                select.innerHTML = options;
+                            }
+                            select.hidden = false;
+                        }
                     }
-                    else {
-                        that.nextElementSibling.innerHTML = options;
-                    }
-                    that.nextElementSibling.focus();
-                })
+                    )
             }, 500);
 
         })
@@ -41,7 +95,7 @@ export default class extends Controller {
 }
 
 async function fetchAsync() {
-    let response = await fetch('https://api-adresse.data.gouv.fr/search/?q=' + that.value + '&autocomplete=1')
+    let response = await fetch('https://api-adresse.data.gouv.fr/search/?q=' + that.value + '&autocomplete=1&limit=' + limit)
     let data = await response.json();
     return data;
 }

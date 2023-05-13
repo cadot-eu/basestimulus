f7fb2bedabd5fccfee9007ac01facbdbc6e900b2
michael <m@cadot.eu>
2023-05-13 09:52:54 +0200

    améliorer(adresse_controller.js) : ajouter la prise en charge de l'autocomplétion d'adresse en utilisant l'API de l'adresse de données gouvernementales. Ajouter un select après l'input pour afficher les résultats de l'autocomplétion. Ajouter un event listener sur le select pour remplir l'input avec la valeur sélectionnée et supprimer le select.

--- adresse_controller.js
+++ adresse_controller.js
@@ -11,9 +11,29 @@ export default class extends Controller {
         this.element.addEventListener('input', function (e) {
             clearTimeout(timer);
             timer = setTimeout(function () {
-                let data = fetchAsync();
-                console.log(data);
-            }, 1000);
+                //on récupère les données de l'api
+                fetchAsync().then(data => {
+
+                    console.log(data);
+                    //on ajoute un select après l'input s'il existe pas sinon on modifie l'existant
+                    let options = '<option value="">Choisir une adresse</option>';
+                    data.features.forEach(element => {
+                        options += '<option value="' + element.properties.label + '">' + element.properties.label + '</option>';
+                    });
+                    if (that.nextElementSibling == null) {
+                        that.insertAdjacentHTML('afterend', '<select class="form-select" aria-label="adresse autocomplete" id="adresse_autocomplete">' + options + '</select>');
+                        //on ajoute un event listener sur le select
+                        that.nextElementSibling.addEventListener('change', function (e) {
+                            that.value = this.value;
+                            this.remove();
+                        });
+                    }
+                    else {
+                        that.nextElementSibling.innerHTML = options;
+                    }
+                    that.nextElementSibling.focus();
+                })
+            }, 500);
 
         })
     }
@@ -21,7 +41,7 @@ export default class extends Controller {
 }
 
 async function fetchAsync() {
-    let response = await fetch('https://nominatim.openstreetmap.org/search?q=' + that.value + '&format=json&limit=5')
+    let response = await fetch('https://api-adresse.data.gouv.fr/search/?q=' + that.value + '&autocomplete=1')
     let data = await response.json();
     return data;
 }

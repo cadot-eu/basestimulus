1144f7ab6b0d1cbf6b24bd5d6de10e254df86643
michael <m@cadot.eu>
2023-05-20 18:46:18 +0200

    style(readonlyroot_controller.js) : ajouter des classes CSS à l'élément parent pour modifier son apparence et le rendre plus visible.

--- readonlyroot_controller.js
+++ readonlyroot_controller.js
@@ -17,7 +17,11 @@ export default class extends Controller {
             divparent.classList.add('mb-3', 'row', 'mt-n3')
             divparent.innerHTML = '<div class="offset-2 col-sm-10"><div class="form-text text-muted">' + this.element.value + '</div></div>';
             this.element.parentNode.parentNode.replaceWith(divparent)
+
         }
+        this.element.parentNode.parentNode.classList.remove('d-none');
+        this.element.parentNode.parentNode.classList.add('mb-3');
+        this.element.parentNode.parentNode.classList.add('text-warning');
     }
 
 

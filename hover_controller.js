50a6e9fcc76ac897b267c473d933f001c86f2716
michael <michael@cadot.eu>
2023-07-09 10:10:00 +0200

    add carousel controller

--- hover_controller.js
+++ hover_controller.js
@@ -1,43 +1,50 @@
-// stimulus pour ajouter une class automatiquement quand l'élément apparait
-//possibilité d'ajouter une loopValue à true pour enlever la class quand l'élément disparait
+// Code: base.hover_controller
+//permet de cacher un element au survol d'un autre
+// par défaut c'est l'élément à cacher est dans le l'élément survoler
+// sinon l'élément à cacher est dans le document
+// la clsse à cacher ou affciher est d-none par défaut
+//exemple
+
+{/*
+ <div class="swiper-slide" data-controller="base--hover" data-base--hover-query-value=".card">
+<div class="swiper-lazy d-flex align-items-end img-bg" data-background="{{ asset(partenaire.photo)|imagine_filter('bandeaufixe') }}">
+<p class="h4 text-white text-center m-2">
+    {{ partenaire.nom }}
+</p>
+<div class="card d-none">
+    <div class="card-header"> 
+*/}
 
 import { Controller } from '@hotwired/stimulus'
 import { useHover } from 'stimulus-use'
+
 export default class extends Controller {
     /* -------------------------------------------------------------------------- */
     /*                              variable and uses                             */
     /* -------------------------------------------------------------------------- */
     static values = {
-        class: String,  // class add when the element appear
-        id: String,
-        loop: String,
-        inverse: Boolean,
-        hidethis: Boolean    // true for remove the class if element disappear
-    }
-
-    options = {
-        threshold: 0, // default
+        class: { type: String, default: "d-none" },
+        query: { type: String, default: "" },//donne la cible
+        internal: { type: Boolean, default: true },//dis si la cible est dans le controller
+        loop: String
     }
 
     connect() {
-        useHover(this, { element: this.element })
+        if (this.queryValue) {
+            if (this.internalValue) this.cible = this.element.querySelector(this.queryValue)
+            else
+                this.cible = document.querySelector(this.queryValue)
+        }
+        else this.cible = this.element
+        useHover(this, { element: this.element });
     }
 
+
     mouseEnter() {
-        if (this.inverseValue)
-            document.getElementById(this.idValue).classList.remove(this.classValue)
-        else
-            document.getElementById(this.idValue).classList.add(this.classValue)
-        if (this.hidethisValue)
-            this.element.style.display = "none";
+        this.cible.classList.remove(this.classValue)
     }
 
     mouseLeave() {
-        if (this.inverseValue)
-            document.getElementById(this.idValue).classList.add(this.classValue)
-        else
-            if (this.loopValue == "true") document.getElementById(this.idValue).classList.remove(this.classValue)
-        if (this.hidethisValue)
-            this.element.style.display = "block";
+        this.cible.classList.add(this.classValue)
     }
 }

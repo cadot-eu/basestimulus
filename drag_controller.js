77ca31e2ca3ee7f651419f290d20515d440e4d85
michael <michael@cadot.eu>
2023-09-20 09:26:33 +0200

    maj

--- drag_controller.js
+++ drag_controller.js
@@ -5,7 +5,8 @@ import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 const notyf = new Notyf();
 
-import Sortable from 'sortable-draggable-js/minified.js';
+// Complete SortableJS (with all plugins)
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 import { ajax } from 'jquery';
 
 export default class extends Controller {
@@ -16,54 +17,117 @@ export default class extends Controller {
 
     connect() {
         const entity = this.entityValue
-        this.element.querySelectorAll(this.queryValue).forEach((item) => {
-            const sortable = new Sortable(item, {
-                itemClass: 'drag-item', // class applies to every sortable element
-                draggingClass: 'drag-dragging', // class will apply when dragging start
-                disabledClass: 'drag-disabled', // class will apply if sorting is disable
-                zoom: 1, // it is viewport zoom value (if have css zoom property)
-                containers: 'drag-container', // comma seperated appendable boxes classes
-                fallBackElement: item.classList.contains('fallback') // it is not sortable, it just append this fallback html
-                    ? `<div class="fallback-element">
-                       <span>I am fallback</span>
-                       </div>`
-                    : null,
-                fallBackClone: false, // if will drop a clone of fallback element
-                onStart: (details) => {
-                    // it will trigger when you start sorting
-                    console.log('start:' + details);
-                },
-                onDrop: (details) => {
-                    // it will trigger after dropped even it is sorted or not
-                    console.log('drop:' + details);
-                },
-                onSort: (details) => {
-                    //on lance le change sur l'entity
-                    const input = (details.startedFrom.element.querySelector('input'))
-                    const label = (details.startedFrom.element.querySelector('label'))
-                    const url = '/admin/changeordre/' + entity + '/' + label.innerText + '/' + details.endedOn.index;
-                    console.log(url);
-                    //on lance un ajax et on vérifie que l'on a true et on affiche un message sinon on on affiche une erreur
-                    ajax({
-                        url: url,
-                        method: 'get',
-                        success: (data) => {
-                            //notyf.success('ordre enregistré');
-                            //on réactualise la page
-                            window.location.reload();
-                        },
-                        error: (data) => {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Erreur',
-                                text: data
-                            })
-                        }
+        var sortable = new Sortable(this.element, {
+            draggable: this.queryValue,  // Specifies which items inside the element should be draggable
 
-                    })
-                }
-            });
+            setData: function (/** DataTransfer */dataTransfer, /** HTMLElement*/dragEl) {
+                dataTransfer.setData('Text', dragEl.textContent); // `dataTransfer` object of HTML5 DragEvent
+            },
+
+            // Element is chosen
+            onChoose: function (/**Event*/evt) {
+                evt.oldIndex;  // element index within parent
+            },
+
+            // Element is unchosen
+            onUnchoose: function (/**Event*/evt) {
+                // same properties as onEnd
+            },
+
+            // Element dragging started
+            onStart: function (/**Event*/evt) {
+                evt.oldIndex;  // element index within parent
+            },
+
+            // Element dragging ended
+            onEnd: function (/**Event*/evt) {
+                var itemEl = evt.item;  // dragged HTMLElement
+                evt.to;    // target list
+                evt.from;  // previous list
+                evt.oldIndex;  // element's old index within old parent
+                evt.newIndex;  // element's new index within new parent
+                evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+                evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+                evt.clone // the clone element
+                evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
+            },
+
+            // Element is dropped into the list from another list
+            onAdd: function (/**Event*/evt) {
+                // same properties as onEnd
+            },
+
+            // Changed sorting within list
+            onUpdate: function (/**Event*/evt) {
+                // same properties as onEnd
+            },
+
+            // Called by any change to the list (add / update / remove)
+            onSort: function (/**Event*/evt) {
+                //on lance le change sur l'entity
+                const input = (evt.item.querySelector('input'))
+                const label = (evt.item.querySelector('label'))
+                const url = '/admin/changeordre/' + entity + '/' + label.innerText + '/' + evt.newIndex;
+                console.log(url);
+                //on lance un ajax et on vérifie que l'on a true et on affiche un message sinon on on affiche une erreur
+                ajax({
+                    url: url,
+                    method: 'get',
+                    success: (data) => {
+                        //notyf.success('ordre enregistré');
+                        //on réactualise la page
+                        //window.location.reload();
+                    },
+                    error: (data) => {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Erreur',
+                            text: data
+                        })
+                    }
+
+                })
+            },
+
+            // Element is removed from the list into another list
+            onRemove: function (/**Event*/evt) {
+                // same properties as onEnd
+            },
+
+            // Attempt to drag a filtered element
+            onFilter: function (/**Event*/evt) {
+                var itemEl = evt.item;  // HTMLElement receiving the `mousedown|tapstart` event.
+            },
+
+            // Event when you move an item in the list or between lists
+            onMove: function (/**Event*/evt, /**Event*/originalEvent) {
+                // Example: https://jsbin.com/nawahef/edit?js,output
+                evt.dragged; // dragged HTMLElement
+                evt.draggedRect; // DOMRect {left, top, right, bottom}
+                evt.related; // HTMLElement on which have guided
+                evt.relatedRect; // DOMRect
+                evt.willInsertAfter; // Boolean that is true if Sortable will insert drag element after target by default
+                originalEvent.clientY; // mouse position
+                // return false; — for cancel
+                // return -1; — insert before target
+                // return 1; — insert after target
+                // return true; — keep default insertion point based on the direction
+                // return void; — keep default insertion point based on the direction
+            },
+
+            // Called when creating a clone of element
+            onClone: function (/**Event*/evt) {
+                var origEl = evt.item;
+                var cloneEl = evt.clone;
+            },
+
+            // Called when dragging element changes position
+            onChange: function (/**Event*/evt) {
+                evt.newIndex // most likely why this event is used is to get the dragging element's current index
+                // same properties as onEnd
+            }
         });
+
     }
 }
 
